5d0e3b7220fb03ca01a9a402f5b37ad772e41b31
ronphobos <sidibearouna71@gmail.com>
2022-04-11 09:14:42 +0000

    ajout controller, route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/generate-barcode', [ProductController::class, 'index'])->name('generate.barcode');
